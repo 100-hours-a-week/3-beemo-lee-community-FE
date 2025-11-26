537ba3fa950d19d89c7b53e7a1cfc001f6981e63
Lee Jihun <ljh09060@gmail.com>
2025-11-26 11:48:53 +0900

    chore: 의존성 삭제

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-// .env 파일이 없으면 운영체제 환경변수 또는 기본값을 사용함.
-// 이렇게 함으로써 docker-compose.yml에 .env파일을 불러올 필요가 없고, 하나의 docker-compose.yml로 로컬, 개발, 프로덕션 모든 환경에서 이미지 빌드 가능.
-// 러너가 ssh터널링으로 인스턴스로 docker-compose.yml 실행시킬 때,러너가 인스턴스에 환경변수를 주입해주면 됨.
-if (fs.existsSync('.env')) {
-  require('dotenv').config({ path: '.env' });
-}
-
-const port = process.env.PORT || 3000;
-
 const express = require('express')
 const app = express()
 const path = require('path')
 
+const port = process.env.PORT || 3000;
 // 정적 파일 서빙
 app.use('/css', express.static(path.join(__dirname, 'public/css')));
 app.use('/js', express.static(path.join(__dirname, 'public/js')));
